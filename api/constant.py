e60a8f2401ebabcbdc1ddf0b8c7b87f30ebc0141
unknown <1465499909@qq.com>
2020-11-24 16:23:33 +0800

    æ初始化æ项目

---
+++ api/constant.py
@@ -0,0 +1,7 @@
+# 常量配置文件
+HEADER = {
+    'Referer': 'https://kyfw.12306.cn/otn/login/init',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+}
+
+
